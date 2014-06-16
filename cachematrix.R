137d515e678292fce24f98da86217c315de29735
Isaacpm <Isaacpm@users.noreply.github.com>
2014-06-16 10:54:05 +0100

    Update cachematrix.R
    Created and commented the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions will cache the calculations of the inverse of a matrix. 
+##As the inverse of a matrix is a costly computation its caching will provide performance benefits
 
-## Write a short comment describing this function
+
+##Creates a matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #Sets the given matrix from the function input to add to the cache list
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  #Gets the matrix input to add to the cache list
+  get <- function() x
+  #Computes the inverse of the matrix and assigns it to variable for input in the cache list
+  setinverse <- function(solve) m <<- solve
+  
+  #Gets the inverse of the matrix from the list
+  getinverse <- function() m
+  #Creates the list of matrix and results for retrieval in the cacheSolve function
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##Computes the inverse of the matrix created by makeCacheMatrix, if the inverse has already been created, 
+##and the matix hasn't changed, it retrieves the result from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#Function gets a matrix as input
+cacheSolve <- function(x=matrix(), ...) {
+  
+  #Checks if the matrix is stored in the cached result list and if it is, it gets the cached data.
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  #If the matrix wasn't stored  in the cached results list, it calculates the inverse and adds it to the list
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
